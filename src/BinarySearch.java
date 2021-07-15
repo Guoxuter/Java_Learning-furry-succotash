821962416788ec801c896e305bc68f42941d55de
guohao <guoxuter@gmail.com>
2021-07-15 20:20:09 +0800

    Default Changelist

---
+++ src/BinarySearch.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class BinarySearch {
+    public static int rank(int key, int[] a) {
+        int lo = 0;
+        int hi = a.length - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (key < a[mid]) hi = mid = 1;
+            else if (key > a[mid]) lo = mid + 1;
+            else return mid;
+        }
+        return -1;
+
+    }
+
+    public static void main(String[] args){
+
+        int[] x={1,2,3,4,5};
+        System.out.println("x:"+args[0]);
+
+
+    }
+}
---
+++ src/Helloworld.java
@@ -0,0 +1,15 @@
+public class Helloworld{
+    public static void main(String[] args){
+        int y=1;
+        System.out.println("Before the loop");
+        while(y<4){
+            System.out.println("Inthe loop");
+            System.out.println("Value of x is"+y);
+            y+=1;
+
+        }
+        System.out.println("after the loop");
+        }
+    }
+
+
