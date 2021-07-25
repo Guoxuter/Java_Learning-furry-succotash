a83e6bbd86b0fb55d5e6a878275f2b31d19d33c5
guohao <guoxuter@gmail.com>
2021-07-25 17:36:35 +0800

    Default Changelist

--- src/Movie.java
+++ src/Movie.java
@@ -1,3 +1,22 @@
 public class Movie {
+    String title;
+    String genre;
+    int rating;
 
+    void playIt(){
+        System.out.println("Playing the movie");
+    }
+
+    public class MovieTestDrive{
+        public static  void main(String[] args){
+            Movie one = new Movie();
+            one.title="Gone with the Stock";
+            one.genre ="Tragic";
+            one.rating=-2;
+            Movie two =new Movie();
+            two.title="Lost in the Cubicle Space";
+            two.genre="Comedy";
+            two.playIt();
+        }
+    }
 }
